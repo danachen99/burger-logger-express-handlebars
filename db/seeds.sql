16a06983c53cd85fbcf581dfa94198cbdc1a0ff5
dc <chendana95@gmail.com>
2020-02-25 02:54:49 -0500

    created db and added values to the table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Atomic Bun", false), ("The Ultimate Pickle",false),("Burger Queen",false),
+("Buntastic",true),("Bunh mi",true),("BUNbelievable",true);

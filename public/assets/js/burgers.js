bdf12203cbf9559785dd609f9433078ae58b0a37
dc <chendana95@gmail.com>
2020-02-27 05:49:36 -0500

    added function in js to make the post request of new burger by taking the values from the handlebars form

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,22 @@
-// $(function() {
-//     $(".")
-// })
+$(function() {
+    $(".create-form").on("submit", event => {
+        event.preventDefault();
+
+        //get burger info from index.handlebars form 
+        const newBurger = {
+            name: $("#bur").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        //POST request 
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newCat
+        }).then(
+            function() {
+                console.log("Added a new burger");
+                location.reload();
+            }
+        );
+    });
+})

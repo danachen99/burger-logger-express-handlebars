8d81c79089a8e598162a0eff67f4aec8c98707ba
dc <chendana95@gmail.com>
2020-02-29 23:24:36 -0500

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 let mysql = require('mysql');
+var connection;
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "yourRootPassword",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "yourRootPassword",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(err => {
     if (err) {
